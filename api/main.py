41b1d29026ad0d8cdec6c3ed8ae89d23f5521cd9
Varsh Vishwakarma <87867677+VarshVishwakarma@users.noreply.github.com>
2026-04-16 18:21:07 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
 @app.get("/")
 async def serve_frontend():
